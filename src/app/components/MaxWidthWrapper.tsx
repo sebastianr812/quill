a65d0507259efaf725117f7f6f2748b844278ebf
Sebastian Rojas <sebastianrojas812@gmail.com>
2023-09-29 14:18:26 -0400

    init: initialized nextjs app, added kinde auth, navbar, landing page, tailwind config customized, shadcn init

---
+++ src/app/components/MaxWidthWrapper.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { cn } from "../lib/utils";
+
+const MaxWidthWrapper = (
+{className, children}: {className?: string, children: React.ReactNode}
+) => {
+   return (
+    <div className={cn("mx-auto w-full max-w-screen-lg px-2.5 md:px-20", className)}>
+        {children}
+    </div>
+    )
+}
+
+export default MaxWidthWrapper;
